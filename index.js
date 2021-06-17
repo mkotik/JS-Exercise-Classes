6cae0e1814ed7fa377d689d2ceb31c8f39f580a9
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-06-17 15:01:47 -0400

    completed task 3

--- index.js
+++ index.js
@@ -117,6 +117,9 @@ class Lambdasian {
     this.age = attributes.age;
     this.location = attributes.location;
   }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
 }
 
 /*
